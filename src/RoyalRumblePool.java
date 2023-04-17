47021cad3587686d6b11e4f6d2f22d55d027fd46
CruzAllSystems <Cruz_Gamer.AllSystems_java@yahoo.com>
2023-04-17 11:19:31 -0500

    -Added file reader and initialize in main method

---
+++ src/RoyalRumblePool.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RoyalRumblePool
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File rumbleFile = new File("RumbleReader.txt");
+        Scanner rumbleReader = new Scanner(rumbleFile);
+    }
+
+}
